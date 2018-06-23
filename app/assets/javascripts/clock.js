bf4a6794181b7e81ae0edc1bb0e272b137a14f08
lightcheap <lightcheap88@yahoo.co.jp>
2018-06-23 15:11:18 +0900

    アラーム機能ひとまず完成

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -2,7 +2,9 @@ var alarm_hour = 0;// アラームセットの時間
 var alarm_minute = 0;// アラームセットの分
 var alarm_flug = 0;//ベルが黄色=１
 var notificationflg = 0;//通知の為のフラグ変数
-
+var timer3minute = 180;//3分タイマーの定数
+var timer5minute = 300;//5分
+//-------------------------------------------------------------------------------
 window.onload = function(){
     // ページ読み込み時に実行したい処理
     // ブラウザが通知をサポートしているか確認する
@@ -23,7 +25,7 @@ window.onload = function(){
       });
   }
 }//function end
-
+//----------------------------------------------------------------------------------
 //アラーム通知表示の関数。
 function alarmNotification(theBody,theTitle) {
     var options = {
@@ -40,7 +42,7 @@ function alarmNotification(theBody,theTitle) {
     setTimeout(n.close.bind(n), 10000); 
   }//function end
 
-// 時計のメインとなる関数
+// 時計のメインとなる関数---------------------------------------------------------------
 function clock()
 {
     var weeks = new Array("Sun","Mon","Thu","Wed","Thr","Fri","Sat");// 曜日を表す各文字列の配列
@@ -68,7 +70,6 @@ function clock()
 
 // 仮の処理として毎時0分に音を鳴らす。出来たらモジュールわけとかする。----
     
-
     var timereport1 = document.getElementById("timereport1");
     var timereport2 = document.getElementById("timereport2");
     var timereport3 = document.getElementById("timereport3");
@@ -79,65 +80,53 @@ function clock()
     var l3 = document.getElementById("l-3");
     var l4 = document.getElementById("l-4");
 
-    //モーダルの選択した内容でネクストアラームの表示を変える
+    //モーダルのどれを選択する？
     if( timereport1.checked == true ){
-        // 1こ目　毎時0分
+        // 1こ目　毎時ｘ分
         l1.style.backgroundColor = "floralwhite";
-        alarm_hour = h + 1;//　時間は１つ繰り上げ
-        alarm_minute ="0"+0;//　分は００分
+        alarm_hour = h ;
+        alarm_minute = alarmSetMaiji();//　分は設定した
+        if( mi >= alarm_minute ){ alarm_hour = h + 1;}
+        if( alarm_minute < 10){alarm_minute = "0" + alarm_minute;}
         if( alarm_hour == 24 ){ alarm_hour = 0; }
         alarm_time.innerHTML = "NEXT ALARM : " + alarm_hour + ":" + alarm_minute;
-        if( alarm_flug == 1 && mi == "00" && s <= "15"){
+        if( alarm_flug == 1 && mi == alarm_minute && s <= "15"){
             alarm();
             notificationflg ++;        
         }
     }else if (timereport1.checked != true ){l1.style.backgroundColor = "#dddddd"; }
-
+    //-------------------------------------------------------------------------
     if ( timereport2.checked == true ){
-        //　２こ目　仮で30分毎
+        //　２こ目　セットして3分後にアラーム
         l2.style.backgroundColor = "floralwhite";
-        var alarm_minutes = ["0"+0,30];
-        if( mi < 30 ){
-            alarm_hour = h;
-            alarm_time.innerHTML = "NEXT ALARM : " + alarm_hour + ":" + alarm_minutes[1];
-        }else{
-            alarm_hour = h + 1;
-            if( alarm_hour == 24 ){ alarm_hour = 0; }
-            alarm_time.innerHTML = "NEXT ALARM : " + alarm_hour + ":" + alarm_minutes[0];
-        }
-        if( alarm_flug == 1 && mi == "00" && s <= "15"){
-            alarm();
-            notificationflg ++;
-        }else if( alarm_flug == 1 && mi == "30" && s <= "15" ){
+        //定数timer3minute 1秒ごとに　-１。 ０になったらアラーム。
+        timer3minute -= 1;
+        if ( alarm_flug == 1 && timer3minute <= 0 ){
             alarm();
             notificationflg ++;
         }
-    }else if (timereport2.checked != true ){l2.style.backgroundColor = "#dddddd";}
+        
 
+    }else if (timereport2.checked != true ){l2.style.backgroundColor = "#dddddd";}　　
+　　//--------------------------------------------------------------------------
     if( timereport3.checked == true ){
-        // 3こ目
+        // 3こ目 セットして５分後
         l3.style.backgroundColor = "floralwhite"; 
-        alarm_minute = 50;
-        if ( mi < 50 ){
-            alarm_hour = h;
-            alarm_time.innerHTML = "NEXT ALARM : " + alarm_hour + ":" + alarm_minute;
-        }else{
-            alarm_hour = h + 1;
-            if( alarm_hour == 24 ){ alarm_hour = 0; }
-            alarm_time.innerHTML = "NEXT ALARM : " + alarm_hour + ":" + alarm_minute;
-        }
-        if( alarm_flug == 1 && mi == "50" && s <= "15"){
+        timer5minute -= 1;
+        if ( alarm_flug == 1 && timer5minute <= 0 ){
             alarm();
             notificationflg ++;
         }
-    
+        
+        
     }else if (timereport3.checked != true ){l3.style.backgroundColor = "#dddddd";}
-
+　　//----------------------------------------------------------------------------
     if( timereport4.checked == true ){
-        //４こ目
+        //４こ目 時刻指定してアラーム鳴らす
         l4.style.backgroundColor = "floralwhite";
-        alarm_hour = 8;
-        alarm_minute = 57;
+        alarm_hour = alarmmezamasiH();
+        alarm_minute = alarmmezamasiM();
+        if( alarm_minute < 10){alarm_minute = "0" + alarm_minute;} 
             alarm_time.innerHTML = "NEXT ALARM : " + alarm_hour + ":" + alarm_minute;
             document.getElementById("t4").innerHTML = alarm_hour + "時" + alarm_minute + "分に設定";
             if( alarm_flug == 1 && h == alarm_hour && mi == alarm_minute && s <= "15"){
@@ -145,8 +134,6 @@ function clock()
                 notificationflg ++;
             }
     }else if (timereport4.checked != true ){l4.style.backgroundColor = "#dddddd";}
-    
-
 }//function end
 
 //中央のベルマークがクリックされるごとに色が変わる
@@ -163,13 +150,13 @@ function bellColorChange(){
         alarm_flug = 0;
     }
 }//function end
-
+//---------------------------------------------------------------------------------
 function alarmSetting(){
     
     fadeIn(document.querySelector('#modal2-overlay'), 100);
     fadeIn(document.querySelector('#modal2-content'), 300);
 }
-
+//--------------------------------------------------------------------------------
 function alarmSettingClear(){
     var modal2Overlay = document.getElementById("modal2-overlay");
     var modal2Content = document.getElementById("modal2-content");
@@ -177,7 +164,7 @@ function alarmSettingClear(){
     modal2Overlay.style.display = "none";
     modal2Content.style.display = "none";
 }
-
+//--------------------------------------------------------------------------------
 function alarm(){
     
     var alarmSound = document.getElementById("sound");
@@ -188,7 +175,7 @@ function alarm(){
     }   
 
 }//function end
-
+//---------------------------------------------------------------------------------
 function fadeIn(node, duration) {
     // display: noneでないときは何もしない
     if (getComputedStyle(node).display !== 'none') return;
@@ -218,9 +205,68 @@ function fadeIn(node, duration) {
       }
     });
 }
-
+//--------------------------------------------------------------------------------
 function alarmSetMaiji(){
-    
+    //アラーム設定1こめの、せってい分を取得する関数
+    var maijiMinute = document.getElementById("maiji").selectedIndex
+    document.getElementById("t1").innerHTML ="毎時"+maijiMinute+"分に設定";
+    return maijiMinute;
+}
+//-------------------------------------------------------------------------------
+function alarmmezamasiM(){
+    //アラーム４つめの目覚まし機能の取得分をリターンする関数
+    var alarmM = document.getElementById("alarmminute").selectedIndex
+    return alarmM;
+}
+//-------------------------------------------------------------------------------
+function alarmmezamasiH(){
+    //アラーム４つめの目覚まし機能の取得時間をリターンする関数
+    var alarmH = document.getElementById("alarmhour").selectedIndex
+    document.getElementById("t4").innerHTML = alarmH + "時" + alarmmezamasiM() + "分に設定";
+    return alarmH;
+}
+//---------------------------------------------------------------------------------
+function clickNowTime(){
+    //この関数が呼び出されたときの分を取り出すだけの関数
+    var now = new Date();
+    var minute = now.getMinutes();
+    return minute;
+}
+//---------------------------------------------------------------------------------
+function alarmConfig2(){
+    //3分後に設定が押されたとき
+    timer3minute = 180;
+    alarm_minute = clickNowTime() + 3;
+    if( alarm_minute < 10){alarm_minute = "0" + alarm_minute;} 
+    var h = new Date().getHours();
+        if( alarm_minute >= 60 ){
+            alarm_minute -= 60;
+            alarm_hour = h + 1;
+            if( alarm_hour == 24 ){ alarm_hour = 0; }
+            alarm_time.innerHTML = "NEXT ALARM : " + alarm_hour + ":" + alarm_minute;
+        }else{
+            alarm_hour = h ;
+            if( alarm_hour == 24 ){ alarm_hour = 0; }
+            alarm_time.innerHTML = "NEXT ALARM : " + alarm_hour + ":" + alarm_minute;
+        }
+}
+//---------------------------------------------------------------------------------
+function alarmConfig3(){
+    //5分後に設定が押されたとき
+    timer5minute = 300;
+    alarm_minute = clickNowTime() + 5;
+    if( alarm_minute < 10){alarm_minute = "0" + alarm_minute;}
+    var h = new Date().getHours();
+    if( alarm_minute >= 60 ){
+        alarm_minute -= 60;
+        alarm_hour = h + 1;
+        if( alarm_hour == 24 ){ alarm_hour = 0; }
+            alarm_time.innerHTML = "NEXT ALARM : " + alarm_hour + ":" + alarm_minute;
+        }else{
+            alarm_hour = h ;
+            if( alarm_hour == 24 ){ alarm_hour = 0; }
+            alarm_time.innerHTML = "NEXT ALARM : " + alarm_hour + ":" + alarm_minute;
+        }
 }
 
 // 上記のclock関数を1000ミリ秒ごと(毎秒)に実行する
